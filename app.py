7cd1c43994c050aa01d97b94c7b9312f746d338e
Pylos7 <nestor1ingles@gmail.com>
2023-06-10 19:56:27 -0400

    Add secret key for session encription with imported secrets module

--- app.py
+++ app.py
@@ -5,16 +5,17 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_login import LoginManager, UserMixin, login_user, current_user, login_required, logout_user
 from flask_migrate import Migrate
+import secrets
 
 from models import User, Task, DueDate, Priority
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Set a secret key for session encryption
+app.config['SECRET_KEY'] = secrets.token_hex(16)  # Set a secret key for session encryption
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'  # Set the database URI
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
